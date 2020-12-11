fc39995a6f9bfde3bdfc6ddae7269075e4c7c714
Preston-Too <toopreston@gmail.com>
2020-12-11 12:15:19 +0300

    added user,roles and reasons model

---
+++ app/models.py
@@ -0,0 +1,69 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+  
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    reason = db.relationship('Reason',backref = 'user',lazy = "dynamic")
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+    def __repr__(self):
+        return f'User {self.username}'
+    
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+
+class Reason(db.Model):
+
+    __tablename__ = 'reason'
+
+    id = db.Column(db.Integer,primary_key = True)
+    reason_id = db.Column(db.Integer)
+    reason = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_reason(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_reason(cls,id):
+        reasons = Reason.query.filter_by(reason_id=id).all()
+        return reasons
